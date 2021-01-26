16b5b8aa8a414833175268ac3aba19014eb6f036
Antony <vontonnie@gmail.com>
2021-01-26 07:26:21 +0300

    Create a Paint Object

--- app/src/main/java/com/androidshowtime/ClippedView.kt
+++ app/src/main/java/com/androidshowtime/ClippedView.kt
@@ -1,9 +1,21 @@
 package com.androidshowtime
 
 import android.content.Context
+import android.graphics.Paint
 import android.util.AttributeSet
 import android.view.View
 
 class ClippedView @JvmOverloads constructor(
         context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0) : View(
-        context, attrs, defStyleAttr) {}
+        context, attrs, defStyleAttr) {
+
+
+            private val paint = Paint().apply {
+
+                strokeWidth = resources.getDimension(R.dimen.strokeWidth)
+                textSize = resources.getDimension(R.dimen.textSize)
+
+                //smooth out edges without affecting the shape
+                isAntiAlias = true
+            }
+        }
